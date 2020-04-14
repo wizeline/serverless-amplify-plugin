3fdccd327e559980b1f5074a7012fffc5f87bc5d
Brett Andrews <brett.j.andrews@gmail.com>
2020-04-14 16:29:20 -0700

    docs(examples/basic): update example ui with Wizeline description

--- examples/basic/src/App.js
+++ examples/basic/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import banner from './wizeline-amplify-serverless-banner.png';
+import banner from './wizeline-amplify-serverless-banner-transparent.png';
 import './App.css';
 
 function App() {
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={banner} className="App-banner" alt="banner" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Developed and maintained by <a href="https://www.wizeline.com/" target="_blank" className="App-link" rel="noopener noreferrer">Wizeline</a>.
+            </p>
+        <p>
+          Wizeline understands that great software is built by great people and teams. If you’d like to partner with Wizeline to build your software or expand your existing team with veteran engineers, project managers, technical writers, <a href="https://www.wizeline.com/contact/" target="_blank" className="App-link" rel="noopener noreferrer">reach out to our team</a>.
+            </p>
       </header>
     </div>
   );
