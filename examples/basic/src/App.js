c82ce75e7eac23b1e94b6796ba7359ee3a4c13d1
Brett Andrews <brett.j.andrews@gmail.com>
2020-04-14 16:05:37 -0700

    docs(examples/basic): update example UI with plugin graphic

--- examples/basic/src/App.js
+++ examples/basic/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import banner from './wizeline-amplify-serverless-banner.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={banner} className="App-banner" alt="banner" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
